0d2e0cdb554ce3642ab8509ea5faeb50fdfb94b8
sht0rmik <valerijzhab@yandex.ru>
2022-04-11 13:32:10 +0300

    Fixed comments after the review of the task of the first sprint.

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -8,6 +8,7 @@
 #include <string>
 #include <utility>
 #include <vector>
+#include <numeric>
 
 using namespace std;
 
@@ -89,7 +90,8 @@ public:
 
         sort(matched_documents.begin(), matched_documents.end(),
             [](const Document& lhs, const Document& rhs) {
-                if (abs(lhs.relevance - rhs.relevance) < 1e-6) {
+                auto discrepancy = 1e-6;
+                if (abs(lhs.relevance - rhs.relevance) < discrepancy) {
                     return lhs.rating > rhs.rating;
                 }
                 else {
@@ -165,10 +167,7 @@ private:
         if (ratings.empty()) {
             return 0;
         }
-        int rating_sum = 0;
-        for (const int rating : ratings) {
-            rating_sum += rating;
-        }
+        int rating_sum = accumulate(ratings.begin(),ratings.end(),0);
         return rating_sum / static_cast<int>(ratings.size());
     }
 
